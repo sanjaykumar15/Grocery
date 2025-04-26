e556340a7f9e943ab56c2c00c327b009717486a6
sanjay <sanjaykumarnsk111777@gmail.com>
2025-04-26 09:05:12 +0530

    feat: DI set up

---
+++ app/src/main/java/com/sanjay/grocery/core/Constants.kt
@@ -0,0 +1,9 @@
+package com.sanjay.grocery.core
+
+object Constants {
+
+    object RealmCons {
+        const val DB_NAME = "grocery.realm"
+    }
+
+}
---
+++ app/src/main/java/com/sanjay/grocery/di/AppModule.kt
@@ -0,0 +1,39 @@
+package com.sanjay.grocery.di
+
+import android.app.Application
+import android.content.Context
+import com.sanjay.grocery.core.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideContext(application: Application): Context = application.applicationContext
+
+    @Singleton
+    @Provides
+    fun providesRealm(@ApplicationContext context: Context): Realm {
+        Realm.init(context)
+        val realmConfiguration =
+            RealmConfiguration.Builder()
+                .name(Constants.RealmCons.DB_NAME)
+                .deleteRealmIfMigrationNeeded()
+                .allowQueriesOnUiThread(true)
+                .allowWritesOnUiThread(true)
+                .build()
+        Realm.setDefaultConfiguration(realmConfiguration)
+        return Realm.getDefaultInstance()
+    }
+
+}
